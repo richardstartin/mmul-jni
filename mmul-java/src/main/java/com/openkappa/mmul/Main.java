1544a02ff251b60d624d27eb298357f0e3889373
Richard Startin <richard@openkappa.co.uk>
2018-03-11 22:22:56 +0000

    aligned direct byte buffer

--- mmul-java/src/main/java/com/openkappa/mmul/Main.java
+++ mmul-java/src/main/java/com/openkappa/mmul/Main.java
@@ -4,6 +4,8 @@ import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
 import java.util.concurrent.ThreadLocalRandom;
 
+import static java.lang.Integer.bitCount;
+
 public class Main {
 
   static {
@@ -12,9 +14,9 @@ public class Main {
 
   public static void main(String[] args) {
     int size = 512;
-    FloatBuffer left = ByteBuffer.allocateDirect(size * size * 4).asFloatBuffer().put(data(size * size));
-    FloatBuffer right = ByteBuffer.allocateDirect(size * size * 4).asFloatBuffer().put(data(size * size));
-    FloatBuffer result = ByteBuffer.allocateDirect(size * size * 4).asFloatBuffer();
+    FloatBuffer left = allocateDirectAligned(size * size * 4, 64).asFloatBuffer().put(data(size * size));
+    FloatBuffer right = allocateDirectAligned(size * size * 4, 64).asFloatBuffer().put(data(size * size));
+    FloatBuffer result = allocateDirectAligned(size * size * 4, 64).asFloatBuffer();
     MatrixMultiplication.multiply(left, right, result, size);
     for (int i = 0; i < size * size; ++i) {
       System.out.println(result.get(i));
@@ -28,5 +30,39 @@ public class Main {
       data[i] = ThreadLocalRandom.current().nextFloat();
     }
     return data;
+  }
+
+
+  /**
+   * Taken from agrona
+   * @param capacity
+   * @param alignment
+   * @return
+   */
+  public static ByteBuffer allocateDirectAligned(final int capacity, final int alignment) {
+    if (bitCount(alignment) != 1) {
+      throw new IllegalArgumentException("Must be a power of 2: alignment=" + alignment);
+    }
+
+    final ByteBuffer buffer = ByteBuffer.allocateDirect(capacity + alignment);
+
+    final long address = address(buffer);
+    final int remainder = (int)(address & (alignment - 1));
+    final int offset = alignment - remainder;
+
+    buffer.limit(capacity + offset);
+    buffer.position(offset);
+
+    return buffer.slice();
+  }
+
+  /**
+   * Get the address at which the underlying buffer storage begins.
+   *
+   * @param buffer that wraps the underlying storage.
+   * @return the memory address at which the buffer storage begins.
+   */
+  public static long address(final ByteBuffer buffer) {
+    return ((sun.nio.ch.DirectBuffer)buffer).address();
   }
 }
--- mmul-native/src/main/native/mmul.cpp
+++ mmul-native/src/main/native/mmul.cpp
@@ -11,33 +11,33 @@ static void mmul_tiled_avx_unrolled(const int n, const float *left, const float 
         for (int row_offset = 0; row_offset < n; row_offset += block_height) {
             for (int i = 0; i < n; ++i) {
                 for (int j = column_offset; j < column_offset + block_width && j < n; j += 64) {
-                    __m256 sum1 = _mm256_loadu_ps(result + i * n + j);
-                    __m256 sum2 = _mm256_loadu_ps(result + i * n + j + 8);
-                    __m256 sum3 = _mm256_loadu_ps(result + i * n + j + 16);
-                    __m256 sum4 = _mm256_loadu_ps(result + i * n + j + 24);
-                    __m256 sum5 = _mm256_loadu_ps(result + i * n + j + 32);
-                    __m256 sum6 = _mm256_loadu_ps(result + i * n + j + 40);
-                    __m256 sum7 = _mm256_loadu_ps(result + i * n + j + 48);
-                    __m256 sum8 = _mm256_loadu_ps(result + i * n + j + 56);
+                    __m256 sum1 = _mm256_load_ps(result + i * n + j);
+                    __m256 sum2 = _mm256_load_ps(result + i * n + j + 8);
+                    __m256 sum3 = _mm256_load_ps(result + i * n + j + 16);
+                    __m256 sum4 = _mm256_load_ps(result + i * n + j + 24);
+                    __m256 sum5 = _mm256_load_ps(result + i * n + j + 32);
+                    __m256 sum6 = _mm256_load_ps(result + i * n + j + 40);
+                    __m256 sum7 = _mm256_load_ps(result + i * n + j + 48);
+                    __m256 sum8 = _mm256_load_ps(result + i * n + j + 56);
                     for (int k = row_offset; k < row_offset + block_height && k < n; ++k) {
                         __m256 multiplier = _mm256_set1_ps(left[i * n + k]);
-                        sum1 = _mm256_fmadd_ps(multiplier, _mm256_loadu_ps(right + k * n + j), sum1);
-                        sum2 = _mm256_fmadd_ps(multiplier, _mm256_loadu_ps(right + k * n + j + 8), sum2);
-                        sum3 = _mm256_fmadd_ps(multiplier, _mm256_loadu_ps(right + k * n + j + 16), sum3);
-                        sum4 = _mm256_fmadd_ps(multiplier, _mm256_loadu_ps(right + k * n + j + 24), sum4);
-                        sum5 = _mm256_fmadd_ps(multiplier, _mm256_loadu_ps(right + k * n + j + 32), sum5);
-                        sum6 = _mm256_fmadd_ps(multiplier, _mm256_loadu_ps(right + k * n + j + 40), sum6);
-                        sum7 = _mm256_fmadd_ps(multiplier, _mm256_loadu_ps(right + k * n + j + 48), sum7);
-                        sum8 = _mm256_fmadd_ps(multiplier, _mm256_loadu_ps(right + k * n + j + 56), sum8);
+                        sum1 = _mm256_fmadd_ps(multiplier, _mm256_load_ps(right + k * n + j), sum1);
+                        sum2 = _mm256_fmadd_ps(multiplier, _mm256_load_ps(right + k * n + j + 8), sum2);
+                        sum3 = _mm256_fmadd_ps(multiplier, _mm256_load_ps(right + k * n + j + 16), sum3);
+                        sum4 = _mm256_fmadd_ps(multiplier, _mm256_load_ps(right + k * n + j + 24), sum4);
+                        sum5 = _mm256_fmadd_ps(multiplier, _mm256_load_ps(right + k * n + j + 32), sum5);
+                        sum6 = _mm256_fmadd_ps(multiplier, _mm256_load_ps(right + k * n + j + 40), sum6);
+                        sum7 = _mm256_fmadd_ps(multiplier, _mm256_load_ps(right + k * n + j + 48), sum7);
+                        sum8 = _mm256_fmadd_ps(multiplier, _mm256_load_ps(right + k * n + j + 56), sum8);
                     }
-                    _mm256_storeu_ps(result + i * n + j, sum1);
-                    _mm256_storeu_ps(result + i * n + j + 8, sum2);
-                    _mm256_storeu_ps(result + i * n + j + 16, sum3);
-                    _mm256_storeu_ps(result + i * n + j + 24, sum4);
-                    _mm256_storeu_ps(result + i * n + j + 32, sum5);
-                    _mm256_storeu_ps(result + i * n + j + 40, sum6);
-                    _mm256_storeu_ps(result + i * n + j + 48, sum7);
-                    _mm256_storeu_ps(result + i * n + j + 56, sum8);
+                    _mm256_store_ps(result + i * n + j, sum1);
+                    _mm256_store_ps(result + i * n + j + 8, sum2);
+                    _mm256_store_ps(result + i * n + j + 16, sum3);
+                    _mm256_store_ps(result + i * n + j + 24, sum4);
+                    _mm256_store_ps(result + i * n + j + 32, sum5);
+                    _mm256_store_ps(result + i * n + j + 40, sum6);
+                    _mm256_store_ps(result + i * n + j + 48, sum7);
+                    _mm256_store_ps(result + i * n + j + 56, sum8);
                 }
             }
         }
